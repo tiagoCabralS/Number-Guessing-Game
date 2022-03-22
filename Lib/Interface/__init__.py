df1e72f3d092f6d262d703d7049cd8fcc93728e8
Tiago Cabral <tiagocabral.gja@gmail.com>
2022-03-21 21:16:08 -0300

    Atualização da def explicação()

--- Lib/Interface/__init__.py
+++ Lib/Interface/__init__.py
@@ -12,5 +12,5 @@ def explicação():
     print('O que é o jogo de adivinhação de números?')
     print('\nO jogo de adivinhação de números, é um jogo simples de adivinhação,\n'
           'onde o usuário deve adivinhar um número entre 0 e N números de acordo com a fase.'
-          '\nO jogo irá acabar depois de 10 tentativas para 5 fases, cada uma ficando mais difícil,'
+          '\nO jogo irá acabar depois de X tentativas de acordo com a fase, foram feitas no total 5 fases,'
           '\nse o jogador falhar em adivinhar o número em qualquer fase, o jogo será finalizado.')
